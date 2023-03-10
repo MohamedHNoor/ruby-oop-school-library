fc0ea5a12bc4147abea5d454af54986a2605c93b
ameerhamzakhalid <ameerhamza.khalid1997@gmail.com>
2023-03-11 00:07:32 +0500

    added bug

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Teacher < Person
   def initialize(id, age, specialization, name = 'unknown')
-    super(id, age, name, parent_permission: true)
+    super(age,id, name, parent_permission: true)
     @specialization = specialization
   end
 
